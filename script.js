02a75641591a0170ffb537e0affb57f47c18fdb6
Ola-Reda <ola.reda.900@gmail.com>
2023-12-22 10:34:34 +0200

    Add new js project

---
+++ script.js
@@ -0,0 +1,6 @@
+const number = document.querySelector(".number")
+const decreaseBtn = document.querySelector(".decrease")
+const resetBtn = document.querySelector(".reset")
+const increaseBtn = document.querySelector(".increase")
+
+console.log(number, decreaseBtn, resetBtn, increaseBtn)
