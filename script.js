f63be1a3ae003704d723c4cf02b0896370c86dad
Ola-Reda <ola.reda.900@gmail.com>
2023-12-22 21:37:39 +0200

    Add new changes

--- script.js
+++ script.js
@@ -3,4 +3,43 @@ const decreaseBtn = document.querySelector(".decrease")
 const resetBtn = document.querySelector(".reset")
 const increaseBtn = document.querySelector(".increase")
 
-console.log(number, decreaseBtn, resetBtn, increaseBtn)
+let count = 0
+number.textContent = count
+
+// increase function
+const increase = () => {
+    count++
+    if(count > 0) {
+        number.style.color = "green"
+    }
+    if(count == 0) {
+        number.style.color = "#222"
+    }
+    number.textContent = count
+}
+increaseBtn.addEventListener("click", increase)
+
+
+// decrease function
+const decrease = () => {
+    count--
+    if(count < 0) {
+        number.style.color = "red"
+    }
+    if(count == 0) {
+        number.style.color = "#222"
+    }
+    number.textContent = count
+}
+decreaseBtn.addEventListener("click", decrease)
+
+
+// reset function
+const reset = () => {
+    count = 0
+    if(count == 0) {
+        number.style.color = "#222"
+    }
+    number.textContent = count
+}
+resetBtn.addEventListener("click", reset)
